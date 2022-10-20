705bf04d0b93404cdf342b6df00d6a4a7a8bb284
Franklin Chua <franklin.chua@gmail.com>
2022-10-20 14:49:39 +0800

    More changes to the contract code

--- animal-shelter-v1/src/AnimalShelter.hs
+++ animal-shelter-v1/src/AnimalShelter.hs
@@ -31,24 +31,29 @@ import           Prelude                (IO, Semigroup (..), Show (..), String)
 import           Text.Printf            (printf)
 import           Wallet.Emulator.Wallet
 
+--- ON-CHAIN ---
+
+-- This is used to parameterized the minting policy so we can control who is allowed to mint
 data PolicyParam = PolicyParam
-    { ppProjectPubKeyHash :: !PaymentPubKeyHash
-    , ppManagerPubKeyHash :: !PaymentPubKeyHash
+    { ppTreasuryPubKeyHash :: !PaymentPubKeyHash -- treasury public key hash
+    , ppManagerPubKeyHash  :: !PaymentPubKeyHash -- manager public key hash
+    , ppProjectName        :: !BuiltinByteString -- additional identifier
     } deriving (Show, Generic)
 
 PlutusTx.makeLift ''PolicyParam
 --PlutusTx.unstableMakeIsData ''PolicyParam
 
-data ShelterRedeemer = Register | Donate
+-- Minting policy actions
+-- We can also add parameters in the future for additional controls
+data ShelterRedeemer = Register | Donate | Unregister
     deriving (Show, Generic)
 PlutusTx.unstableMakeIsData ''ShelterRedeemer
 
---{-# INLINABLE params1 #-}
-params1 :: PolicyParam
-params1 = PolicyParam 
-    { ppProjectPubKeyHash = PaymentPubKeyHash "82669eddc629c8ce5cc3cb908cec6de339281bb0a0ec111880ff0936132ac8b0" 
-    , ppManagerPubKeyHash = PaymentPubKeyHash "82669eddc629c8ce5cc3cb908cec6de339281bb0a0ec111880ff0936132ac8b0" 
-    }
+--params1 :: PolicyParam
+--params1 = PolicyParam 
+--    { ppTreasuryPubKeyHash = PaymentPubKeyHash "82669eddc629c8ce5cc3cb908cec6de339281bb0a0ec111880ff0936132ac8b0" 
+--    , ppManagerPubKeyHash = PaymentPubKeyHash "82669eddc629c8ce5cc3cb908cec6de339281bb0a0ec111880ff0936132ac8b0" 
+--    }
 
 -- traceIfFalse "wrong amount minted" checkMintedAmount &&
 
@@ -95,6 +100,7 @@ mkPolicy pp redeemer ctx =
     checkMintedAmount :: Bool
     checkMintedAmount = True
 
+-- function to generate the parameterized minting policy script
 policy :: PolicyParam -> Scripts.MintingPolicy
 policy p = mkMintingPolicyScript $
         $$(PlutusTx.compile [|| wrap ||])
@@ -103,100 +109,155 @@ policy p = mkMintingPolicyScript $
     where
         wrap mp' = Scripts.wrapMintingPolicy $ mkPolicy mp'
 
+-- function to generate the currency symbol using the parametarized minting policy script
 curSymbol :: PolicyParam -> CurrencySymbol
 curSymbol p = scriptCurrencySymbol $ policy p
 
-data NFTParams = NFTParams
-    { npToken   :: !TokenName
-    , npAddress :: !Address
-    , npPolicyParam :: !PolicyParam
+--- OFF-CHAIN ---
+
+data RegisterParams = RegisterParams
+    { rpToken       :: !TokenName -- base token name, how to guarantee uniqueness?
+    , rpAddress     :: !Address -- can we auto-detect this inside the Contract?
+    , rpDescription :: !BuiltinByteString
+    , rpPolicyParam :: !PolicyParam
     } deriving (Generic, FromJSON, ToJSON, Show)
 
-type NFTSchema = 
-        Endpoint "register"   NFTParams
-    .\/ Endpoint "donate"     NFTParams
-    .\/ Endpoint "unregister" NFTParams
+data DonateParams = DonateParams
+    { dpToken       :: !TokenName
+    , dpAddress     :: !Address
+    , dpPolicyParam :: !PolicyParam
+    , npDonation    :: !Integer
+    } deriving (Generic, FromJSON, ToJSON, Show)
 
-register :: NFTParams -> Contract w NFTSchema Text ()
-register np = do
+type ShelterSchema = 
+        Endpoint "register"   RegisterParams
+    .\/ Endpoint "donate"     DonateParams
+    .\/ Endpoint "unregister" RegisterParams
+
+register :: RegisterParams -> Contract w ShelterSchema Text ()
+register rp = do
     Contract.logInfo @String "Registering animal..."
-    utxos <- utxosAt $ npAddress np
+    utxos <- utxosAt $ rpAddress rp -- get utxos from the wallet
     case Map.keys utxos of
         []       -> Contract.logError @String "no utxo found"
-        oref : _ -> do
-            let tn      = npToken np
+        oref : _ -> do -- at least 1 utxo found
+            let tn      = rpToken rp
+                pp      = rpPolicyParam rp -- policy parameter
                 refTn   = TokenName $ (appendByteString "(100)" (unTokenName tn))
                 usrTn   = TokenName $ (appendByteString "(222)" (unTokenName tn))
                 r       = Redeemer $ PlutusTx.toBuiltinData Register
-                val     = Value.singleton (curSymbol params1) refTn 1 <> Value.singleton (curSymbol params1) usrTn 1
-                lookups = Constraints.mintingPolicy (policy params1) <> Constraints.unspentOutputs utxos
-                tx      = Constraints.mustMintValueWithRedeemer r val <> Constraints.mustSpendPubKeyOutput oref
+                mintVal = Value.singleton (curSymbol pp) refTn 1 <>
+                          Value.singleton (curSymbol pp) usrTn 1
+                lookups = Constraints.mintingPolicy (policy pp) <>
+                          Constraints.unspentOutputs utxos
+                tx      = Constraints.mustMintValueWithRedeemer r mintVal <>
+                          Constraints.mustPayToTheScript () mintVal <> -- is this the way to send the tokens to the script after minting
+                          Constraints.mustSpendPubKeyOutput oref -- not sure if this is needed, where will this go?
             ledgerTx <- submitTxConstraintsWith @Void lookups tx
             void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
             Contract.logInfo @String $ printf "forged %s" (show val)
 
-donate :: NFTParams -> Contract w NFTSchema Text ()
+unregister :: RegisterParams -> Contract w ShelterSchema Text ()
+unregister rp = do
+    Contract.logInfo @String "Unregistering animal..."
+    utxos <- utxosAt $ rpAddress rp -- get utxos from the wallet
+    case Map.keys utxos of
+        []       -> Contract.logError @String "no utxo found"
+        oref : _ -> do -- at least 1 utxo found
+            let tn      = rpToken rp
+                pp      = rpPolicyParam rp -- policy parameter
+                refTn   = TokenName $ (appendByteString "(100)" (unTokenName tn))
+                usrTn   = TokenName $ (appendByteString "(222)" (unTokenName tn))
+                r       = Redeemer $ PlutusTx.toBuiltinData Register
+                mintVal = Value.singleton (curSymbol pp) refTn (- 1) <>
+                          Value.singleton (curSymbol pp) usrTn (- 1)
+                lookups = Constraints.mintingPolicy (policy pp) <>
+                          Constraints.unspentOutputs utxos
+                tx      = Constraints.mustMintValueWithRedeemer r mintVal <>
+                          Constraints.mustSpendPubKeyOutput oref -- not sure if this is needed, where will this go?
+            ledgerTx <- submitTxConstraintsWith @Void lookups tx
+            void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+            Contract.logInfo @String $ printf "forged %s" (show val)
+
+donate :: DonateParams -> Contract w ShelterSchema Text ()
 donate np = do
     Contract.logInfo @String "Sending donation..."
-    utxos <- utxosAt $ npAddress np
+    utxos <- utxosAt $ dpAddress np
     case Map.keys utxos of
         []       -> Contract.logError @String "no utxo found"
         oref : _ -> do
-            let tn      = npToken np
-                pp      = npPolicyParam np
-            let val     = Value.singleton (curSymbol pp) "(333) Frank" 1
-                lookups = Constraints.mintingPolicy (policy pp) <> Constraints.unspentOutputs utxos
-                tx      = Constraints.mustMintValue val <> Constraints.mustSpendPubKeyOutput oref
+            let tn          = dpToken np
+                pp          = dpPolicyParam np
+                donTn       = TokenName $ (appendByteString "(333)" (unTokenName tn))
+                tPkh        = ppTreasuryPubKeyHash pp
+                donation    = npDonation pp
+                mintVal     = Value.singleton (curSymbol pp) donTn 1 -- mint donation token
+                donationVal = (Ada.lovelaceValueOf donation)
+                lookups     = Constraints.mintingPolicy (policy pp) <>
+                              Constraints.unspentOutputs utxos
+                tx          = Constraints.mustMintValue mintVal <>
+                              Constraints.mustPayToPubKey tPkh donationVal -- send donation to treasury wallet
             ledgerTx <- submitTxConstraintsWith @Void lookups tx
             void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
-            Contract.logInfo @String $ printf "forged %s" (show val)            
+            Contract.logInfo @String $ printf "forged %s" (show val)
 
-endpoints :: Contract () NFTSchema Text ()
-endpoints = awaitPromise (register' `select` donate') >> endpoints
+endpoints :: Contract () ShelterSchema Text ()
+endpoints = awaitPromise (register' `select` donate' `select` unregister') >> endpoints
   where
-    register' = endpoint @"register" register
-    donate'   = endpoint @"donate"   donate
+    register'   = endpoint @"register"   register
+    donate'     = endpoint @"donate"     donate
+    unregister' = endpoint @"unregister" unregister
 
 test :: IO ()
 test = runEmulatorTraceIO $ do
     let tn = "ABC"
-        w1 = knownWallet 1 -- treasury
-        w2 = knownWallet 2 -- manager
-        w3 = knownWallet 3 -- donor
-        w4 = knownWallet 4 -- donor
-        w5 = knownWallet 5 -- donor
-        pkhT = mockWalletPaymentPubKeyHash w1
-        pkhM = mockWalletPaymentPubKeyHash w2
-        paramP = PolicyParam
-            { ppProjectPubKeyHash = pkhT
-            , ppManagerPubKeyHash = pkhM
-            }
+        w1       = knownWallet 1 -- treasury wallet
+        w2       = knownWallet 2 -- manager wallet
+        w3       = knownWallet 3 -- donor
+        w4       = knownWallet 4 -- donor
+        w5       = knownWallet 5 -- donor
+        pkhT     = mockWalletPaymentPubKeyHash w1
+        pkhM     = mockWalletPaymentPubKeyHash w2
+        paramPol = PolicyParam
+                 { ppTreasuryPubKeyHash = pkhT
+                 , ppManagerPubKeyHash = pkhM
+                 , ppProjectName = "Hound Haven"
+                 }
+
     h1 <- activateContractWallet w1 endpoints
     h2 <- activateContractWallet w2 endpoints
     h3 <- activateContractWallet w3 endpoints
     h4 <- activateContractWallet w4 endpoints
     h5 <- activateContractWallet w5 endpoints
 
-    callEndpoint @"register" h2 $ NFTParams
-        { npToken       = tn
-        , npAddress     = mockWalletAddress w2
-        , npPolicyParam = paramP
+    callEndpoint @"register" h2 $ RegisterParams
+        { rpToken       = tn
+        , rpAddress     = mockWalletAddress w2
+        , rpDescription = "Female/White"
+        , rpPolicyParam = paramPol
         }
 
     void $ Emulator.waitNSlots 2
     
-    callEndpoint @"donate" h3 $ NFTParams
-        { npToken       = tn
-        , npAddress     = mockWalletAddress w3
-        , npPolicyParam = paramP
+    callEndpoint @"donate" h3 $ DonateParams
+        { dpToken       = tn
+        , dpAddress     = mockWalletAddress w3
+        , dpPolicyParam = paramPol
         }
     
     void $ Emulator.waitNSlots 2
 
-    callEndpoint @"donate" h4 $ NFTParams
-        { npToken       = tn
-        , npAddress     = mockWalletAddress w4
-        , npPolicyParam = paramP
+    callEndpoint @"donate" h4 $ DonateParams
+        { dpToken       = tn
+        , dpAddress     = mockWalletAddress w4
+        , dpPolicyParam = paramPol
         }
-    
+
     void $ Emulator.waitNSlots 2
+
+    callEndpoint @"unregister" h2 $ RegisterParams
+        { rpToken       = tn
+        , rpAddress     = mockWalletAddress w2
+        , rpDescription = "Female/White"
+        , rpPolicyParam = paramPol
+        }
